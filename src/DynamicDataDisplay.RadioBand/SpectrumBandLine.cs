0f6196b252abc24190192aae1faafb878e13d4da
John Stewien <stewienj@users.noreply.github.com>
2021-05-23 13:06:18 +0930

    Wrote some of the code for a vertical spectrum overlay on the radio band chart

---
+++ src/DynamicDataDisplay.RadioBand/SpectrumBandLine.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DynamicDataDisplay.RadioBand
+{
+	/// <summary>
+	/// SpectrumBandLine objects to be rendered by the FrequencyRangeLineRenderer.
+	/// These are just data objects, but I add them as children to a panel so bindings can be
+	/// applied. The direct parent is a Canvas type. Note the "AffectsParentMeasure = true"
+	/// on the dependency properties. This forces a render pass in the parent if the property
+	/// changes.
+	/// </summary>
+	public class SpectrumBandLine : FrameworkElement
+	{
+		private Size _zeroSize = new Size(0, 0);
+		public SpectrumBandLine()
+		{
+			// This isn't visible anyway, so just turn stuff off
+			IsHitTestVisible = false;
+			Visibility = Visibility.Collapsed;
+		}
+
+		protected override Size MeasureOverride(Size availableSize)
+		{
+			return _zeroSize;
+		}
+
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			return _zeroSize;
+		}
+
+
+		/// <summary>
+		/// Frequency is the position of the line
+		/// </summary>
+		public double Frequency
+		{
+			get { return (double)GetValue(FrequencyProperty); }
+			set { SetValue(FrequencyProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for Start.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty FrequencyProperty =
+			DependencyProperty.Register("Frequency", typeof(double), typeof(SpectrumBandLine), new FrameworkPropertyMetadata{ DefaultValue = 0.0, AffectsParentMeasure = true, PropertyChangedCallback = new PropertyChangedCallback((s,e)=>
+			{
+				if (s is SpectrumBandLine spectrumBandLine)
+                {
+					spectrumBandLine.BrushNeedsRecaculating = true;
+				}
+			})});
+
+
+		/// <summary>
+		/// Bandwidth is the thickness of the line
+		/// </summary>
+		public double Bandwidth
+		{
+			get { return (double)GetValue(BandwidthProperty); }
+			set { SetValue(BandwidthProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for End.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty BandwidthProperty =
+			DependencyProperty.Register("Bandwidth", typeof(double), typeof(SpectrumBandLine), new FrameworkPropertyMetadata { DefaultValue = 0.0, AffectsParentMeasure = true,
+				PropertyChangedCallback = new PropertyChangedCallback((s, e) =>
+				{
+					if (s is SpectrumBandLine spectrumBandLine)
+					{
+						spectrumBandLine.BrushNeedsRecaculating = true;
+					}
+				})
+			});
+
+		public bool IsSelected
+		{
+			get { return (bool)GetValue(IsSelectedProperty); }
+			set { SetValue(IsSelectedProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for IsSelected.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty IsSelectedProperty =
+			DependencyProperty.Register("IsSelected", typeof(bool), typeof(SpectrumBandLine), new FrameworkPropertyMetadata { DefaultValue = false, AffectsParentMeasure = true, BindsTwoWayByDefault = true });
+
+		/// <summary>
+		/// Line Brush, which will be a gradient brush that has to be calculated for a Log scale
+		/// </summary>
+		internal Brush LineBrush 
+		{
+			get => _lineBrush;
+			set
+            {
+				_lineBrush = value;
+				BrushNeedsRecaculating = false;
+            }
+		}
+		private Brush _lineBrush = new SolidColorBrush(Colors.Blue);
+
+		/// <summary>
+		/// Recalculate the LineBrush which will need doing whenever the frequency or bandwidth changes because it is being displayed on a log scale.
+		/// </summary>
+		internal bool BrushNeedsRecaculating { get; private set; } = false;
+	}
+
+}
---
+++ src/DynamicDataDisplay.RadioBand/SpectrumBandVerticalLineRenderer.xaml.cs
@@ -0,0 +1,174 @@
+﻿using DynamicDataDisplay;
+using DynamicDataDisplay.Charts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DynamicDataDisplay.RadioBand
+{
+	/// <summary>
+	/// Interaction logic for SpectrumBandVerticalLineRenderer.xaml
+	/// </summary>
+	public partial class SpectrumBandVerticalLineRenderer : CanvasGraph
+	{
+		private bool _lineReassignmentRequired = false;
+
+		public SpectrumBandVerticalLineRenderer()
+		{
+			SnapsToDevicePixels = true;
+			InitializeComponent();
+		}
+
+		public void SelectAllAt(double coord)
+		{
+			foreach (var line in GetLines())
+			{
+				line.IsSelected = (line.Frequency-line.Bandwidth*0.5) < coord && coord < (line.Frequency+line.Bandwidth*0.5);
+			}
+		}
+
+		protected override Size MeasureOverride(Size constraint)
+		{
+			this.InvalidateVisual();
+			return base.MeasureOverride(constraint);
+		}
+
+		protected override void OnViewportPropertyChanged(DynamicDataDisplay.ExtendedPropertyChangedEventArgs e)
+		{
+			InvalidateVisual();
+		}
+
+		protected override void OnRender(DrawingContext dc)
+		{
+			if (Plotter2D == null) return;
+
+			if (_lineReassignmentRequired)
+            {
+				ReassignLines();
+				_lineReassignmentRequired = false;
+			}
+
+			// Viewport is the range of the data normalized to 1x1 square coordinates
+			var corner1 = new Point(0, 0).ViewportToData(Plotter2D.Transform);
+			var corner2 = new Point(1, 1).ViewportToData(Plotter2D.Transform);
+
+			var bounds = new DataRect(corner1, corner2);
+
+			Viewport2D.SetContentBounds(this, bounds);
+			ViewportPanel.SetViewportBounds(this, bounds);
+
+			bool exportMode = false;
+			if (Plotter2D is ChartPlotter p)
+			{
+				exportMode = p.ExportMode;
+			}
+
+			foreach (var line in GetLines())
+			{
+				Point leftPoint = new Point(line.Start, line.GroupAxisCoord).DataToScreen(Plotter2D.Transform);
+				Point rightPoint = new Point(line.End, line.GroupAxisCoord).DataToScreen(Plotter2D.Transform);
+				if (leftPoint.X > rightPoint.X)
+				{
+					var temp = leftPoint;
+					leftPoint = rightPoint;
+					rightPoint = temp;
+				}
+
+				double minThickness = line.IsSelected ? 10.0 : 5.0;
+				var leftRightPadding = Math.Max(minThickness * 0.5 - (rightPoint.X - leftPoint.X) * minThickness, exportMode ? 10 : 0);
+				leftPoint = new Point(leftPoint.X - leftRightPadding, Math.Round(leftPoint.Y));
+				rightPoint = new Point(rightPoint.X + leftRightPadding, Math.Round(rightPoint.Y));
+
+				dc.DrawLine(line.IsSelected ? _rangePenSelected : _rangePen, leftPoint, rightPoint);
+
+				/*
+				if (exportMode)
+				{
+					var text = new FormattedText(line.Text, System.Globalization.CultureInfo.CurrentUICulture, FlowDirection.LeftToRight, new Typeface("Verdana"), 12, Brushes.Black);
+
+					dc.DrawText(text, leftPoint + 0.5 * (rightPoint - leftPoint) - new Vector(0.5 * text.Width, 0.5 * text.Height));
+				}
+				*/
+			}
+		}
+
+        private void ReassignLines()
+        {
+			var sourceLines = ItemsSource.OfType<object>().ToList();
+			var chartLines = GetLines().ToList();
+
+			// Remove excess chart lines
+			
+			while(chartLines.Count > sourceLines.Count)
+            {
+				int removeIndex = chartLines.Count - 1;
+				var toRemove = chartLines[removeIndex];
+				chartLines.RemoveAt(removeIndex);
+				Children.Remove(toRemove);
+            }
+
+			// Assign data context to the remaining chart lines
+			
+			for(int i=0; i< chartLines.Count; ++i)
+            {
+				chartLines[i].DataContext = sourceLines[i];
+            }
+
+			// Add in new chart lines if required
+			while(chartLines.Count < sourceLines.Count)
+            {
+				var newLine = new SpectrumBandLine();
+				newLine.DataContext = sourceLines[chartLines.Count];
+				chartLines.Add(newLine);
+				Children.Add(newLine);
+            }
+        }
+
+        private IEnumerable<SpectrumBandLine> GetLines() => Children.OfType<SpectrumBandLine>();
+
+		/// <summary>
+		/// This covers all Dependency Property changes. Just force an update.
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="e"></param>
+		private static void UpdateAndSubscribe(DependencyObject s, DependencyPropertyChangedEventArgs e)
+		{
+			if (s is SpectrumBandVerticalLineRenderer control)
+			{
+				control.InvalidateVisual();
+
+				if (e.OldValue is INotifyCollectionChanged oldCollection)
+				{
+					oldCollection.CollectionChanged -= control.Source_CollectionChanged;
+				}
+				if (e.NewValue is INotifyCollectionChanged newCollection)
+				{
+					newCollection.CollectionChanged += control.Source_CollectionChanged;
+				}
+			}
+		}
+
+		private void Source_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.DataBind, (Action)(() => {
+				_lineReassignmentRequired = true;
+				InvalidateVisual();
+			}));
+		}
+
+		public IEnumerable ItemsSource
+		{
+			get => (IEnumerable)GetValue(ItemsSourceProperty);
+			set => SetValue(ItemsSourceProperty, value);
+		}
+
+		// Using a DependencyProperty as the backing store for ExternalSelectedItemsSource.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ItemsSourceProperty =
+			DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable), typeof(SpectrumBandVerticalLineRenderer), new PropertyMetadata(null, UpdateAndSubscribe));
+
+	}
+}
