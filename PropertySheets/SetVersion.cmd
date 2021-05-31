d45be7b23c20108e4e815cf1baa1b5b7d4cdb9b5
John Stewien <stewienj@users.noreply.github.com>
2021-05-31 17:49:58 +0930

    Fixed an issue on the package restore stage of release

--- PropertySheets/SetVersion.cmd
+++ PropertySheets/SetVersion.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo // This file is auto-generated 
 echo [assembly: System.Reflection.AssemblyCompany("John Stewien")] 
-echo [assembly: System.Reflection.AssemblyProduct("https://github.com/stewienj/DynamicDataDisplayReloaded")] 
+echo [assembly: System.Reflection.AssemblyProduct("DynamicDataDisplayReloaded")] 
 echo [assembly: System.Reflection.AssemblyFileVersion("%1%")] 
 echo [assembly: System.Reflection.AssemblyVersion("%1%")] 
