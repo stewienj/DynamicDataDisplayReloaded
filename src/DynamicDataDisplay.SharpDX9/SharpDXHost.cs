102dfaaf3a06721cbaeea51248fa440f2d45d6e6
John Stewien <stewienj@users.noreply.github.com>
2022-04-29 16:39:32 +0930

    Fixed an issue where the DirectX rendering keeps on going even when not visible.

--- src/DynamicDataDisplay.SharpDX9/SharpDXHost.cs
+++ src/DynamicDataDisplay.SharpDX9/SharpDXHost.cs
@@ -8,198 +8,226 @@ using System.Windows.Interop;
 
 namespace DynamicDataDisplay.SharpDX9
 {
-	public class SharpDXHost : FrameworkElement, IPlotterElement
-	{
-		private D3DImage _image = new D3DImage();
-		private bool _sizeChanged;
-		private PresentParameters _pp = new PresentParameters();
-		private ThrottledAction _resizeAction = new ThrottledAction(TimeSpan.FromMilliseconds(1000));
-		private Direct3DEx _direct3D;
+    public class SharpDXHost : FrameworkElement, IPlotterElement
+    {
+        private D3DImage _image = new D3DImage();
+        private bool _sizeChanged;
+        private PresentParameters _pp = new PresentParameters();
+        private ThrottledAction _resizeAction = new ThrottledAction(TimeSpan.FromMilliseconds(1000));
+        private Direct3DEx _direct3D;
 
-		public Device Device { get; private set; }
+        private bool _compositionTargetRenderingHandlerAdded = false;
 
-		protected override void OnInitialized(EventArgs e)
-		{
-			base.OnInitialized(e);
-		}
+        public Device Device { get; private set; }
 
-		protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
-		{
-			_resizeAction.InvokeAction(() => _sizeChanged = true);
-			base.OnRenderSizeChanged(sizeInfo);
-		}
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+        }
 
-		public bool LockImage()
-		{
-			return _image.TryLock(_timeOutDuration);
-		}
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            _resizeAction.InvokeAction(() => _sizeChanged = true);
+            base.OnRenderSizeChanged(sizeInfo);
+        }
 
-		public void UnlockImage()
-		{
-			_image.Unlock();
-		}
 
-		public void AddChild(object child) => AddLogicalChild(child);
+        public bool LockImage()
+        {
+            return _image.TryLock(_timeOutDuration);
+        }
 
-		public void RemoveChild(object child) => RemoveLogicalChild(child);
+        public void UnlockImage()
+        {
+            _image.Unlock();
+        }
 
-		private void Initialize3D()
-		{
-			HwndSource hwnd = new HwndSource(0, 0, 0, 0, 0, "D3", IntPtr.Zero);
+        public void AddChild(object child) => AddLogicalChild(child);
 
-			_pp.SwapEffect = SwapEffect.Discard;
-			_pp.DeviceWindowHandle = hwnd.Handle;
-			_pp.Windowed = true;
-			_pp.EnableAutoDepthStencil = true;
-			_pp.BackBufferWidth = Math.Max(100, (int)ActualWidth);
-			_pp.BackBufferHeight = Math.Max(100, (int)ActualHeight);
-			_pp.BackBufferFormat = Format.A8R8G8B8;
-			_pp.AutoDepthStencilFormat = Format.D32SingleLockable;
-			_pp.BackBufferCount = 1;
-			try
-			{
-				_direct3D = new Direct3DEx();
-				Device = new DeviceEx(_direct3D, 0, DeviceType.Hardware, hwnd.Handle, CreateFlags.HardwareVertexProcessing, _pp);
-			}
-			catch
-			{
-				// At this point we are pretty much screwed, require DeviceEx for the reset capability
-				MessageBox.Show("Can't start DirectX, so that pretty much discounts doing anything else");
-			}
+        public void RemoveChild(object child) => RemoveLogicalChild(child);
 
-			//System.Windows.Media.CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
-			CompositionTargetRenderingEventManager.AddHandler(CompositionTarget_Rendering);
-		}
+        private void Initialize3D()
+        {
+            HwndSource hwnd = new HwndSource(0, 0, 0, 0, 0, "D3", IntPtr.Zero);
 
-		bool renderNext = true;
+            _pp.SwapEffect = SwapEffect.Discard;
+            _pp.DeviceWindowHandle = hwnd.Handle;
+            _pp.Windowed = true;
+            _pp.EnableAutoDepthStencil = true;
+            _pp.BackBufferWidth = Math.Max(100, (int)ActualWidth);
+            _pp.BackBufferHeight = Math.Max(100, (int)ActualHeight);
+            _pp.BackBufferFormat = Format.A8R8G8B8;
+            _pp.AutoDepthStencilFormat = Format.D32SingleLockable;
+            _pp.BackBufferCount = 1;
+            try
+            {
+                _direct3D = new Direct3DEx();
+                Device = new DeviceEx(_direct3D, 0, DeviceType.Hardware, hwnd.Handle, CreateFlags.HardwareVertexProcessing, _pp);
+            }
+            catch
+            {
+                // At this point we are pretty much screwed, require DeviceEx for the reset capability
+                MessageBox.Show("Can't start DirectX, so that pretty much discounts doing anything else");
+            }
+        }
+
+        bool renderNext = true;
         // Note that if you exceed 2 seconds per frame render, then it will stop updating.
-		Duration _timeOutDuration = new Duration(TimeSpan.FromMilliseconds(2000));
-		private void CompositionTarget_Rendering(object sender, EventArgs e)
-		{
-			renderNext = !renderNext;
-			if (renderNext)
-			{
-				return;
-			}
-			if (_image == null) return;
+        Duration _timeOutDuration = new Duration(TimeSpan.FromMilliseconds(2000));
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            renderNext = !renderNext;
+            if (renderNext)
+            {
+                return;
+            }
+            if (_image == null) return;
 
-			try
-			{
-				// Lock the image while we are resetting as the rendering system freezes when we get a clash
-				if (_sizeChanged && _image.TryLock(_timeOutDuration))
-				{
-					try
-					{
-						HwndSource hwnd = new HwndSource(0, 0, 0, 0, 0, "D3", IntPtr.Zero);
-						_pp.DeviceWindowHandle = hwnd.Handle;
-						_pp.BackBufferWidth = Math.Max(100, (int)ActualWidth);
-						_pp.BackBufferHeight = Math.Max(100, (int)ActualHeight);
-						Device.Reset(_pp);
-						_sizeChanged = false;
-					}
-					finally
-					{
-						_image.Unlock();
-					}
-					return;
-				}
+            try
+            {
+                // Lock the image while we are resetting as the rendering system freezes when we get a clash
+                if (_sizeChanged && _image.TryLock(_timeOutDuration))
+                {
+                    try
+                    {
+                        HwndSource hwnd = new HwndSource(0, 0, 0, 0, 0, "D3", IntPtr.Zero);
+                        _pp.DeviceWindowHandle = hwnd.Handle;
+                        _pp.BackBufferWidth = Math.Max(100, (int)ActualWidth);
+                        _pp.BackBufferHeight = Math.Max(100, (int)ActualHeight);
+                        Device.Reset(_pp);
+                        _sizeChanged = false;
+                    }
+                    finally
+                    {
+                        _image.Unlock();
+                    }
+                    return;
+                }
 
-				if (_image.IsFrontBufferAvailable)
-				{
-					if (!_image.TryLock(_timeOutDuration))
-					{
-						return;
-					}
-					Result result = Device.TestCooperativeLevel();
-					if (result.Failure)
-					{
-						throw new SharpDXException();
-					}
-					try
-					{
-						Device.SetRenderState(global::SharpDX.Direct3D9.RenderState.CullMode, Cull.None);
-						Device.SetRenderState(global::SharpDX.Direct3D9.RenderState.ZEnable, true);
-						Device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Transparent, float.MaxValue, 0);
-						Device.BeginScene();
-						Render.Raise(this, new RenderEventArgs(_pp.BackBufferWidth, _pp.BackBufferHeight));
-						Device.EndScene();
-						Device.Present();
+                if (_image.IsFrontBufferAvailable)
+                {
+                    if (!_image.TryLock(_timeOutDuration))
+                    {
+                        return;
+                    }
+                    Result result = Device.TestCooperativeLevel();
+                    if (result.Failure)
+                    {
+                        throw new SharpDXException();
+                    }
+                    try
+                    {
+                        Device.SetRenderState(global::SharpDX.Direct3D9.RenderState.CullMode, Cull.None);
+                        Device.SetRenderState(global::SharpDX.Direct3D9.RenderState.ZEnable, true);
+                        Device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Transparent, float.MaxValue, 0);
+                        Device.BeginScene();
+                        Render.Raise(this, new RenderEventArgs(_pp.BackBufferWidth, _pp.BackBufferHeight));
+                        Device.EndScene();
+                        Device.Present();
 
-						_image.SetBackBuffer(D3DResourceType.IDirect3DSurface9, Device.GetBackBuffer(0, 0).NativePointer);
-						_image.AddDirtyRect(new Int32Rect(0, 0, _image.PixelWidth, _image.PixelHeight));
-					}
-					catch (Exception exc)
-					{
-						Debug.WriteLine("Error in rendering in DirectXHost: " + exc.Message);
-					}
-					finally
-					{
-						_image.Unlock();
-					}
-				}
-			}
-			catch (SharpDXException exc)
-			{
-				Device.Reset(_pp);
-				Debug.WriteLine("Exception in main render loop: " + exc.Message);
-			}
-		}
+                        _image.SetBackBuffer(D3DResourceType.IDirect3DSurface9, Device.GetBackBuffer(0, 0).NativePointer);
+                        _image.AddDirtyRect(new Int32Rect(0, 0, _image.PixelWidth, _image.PixelHeight));
+                    }
+                    catch (Exception exc)
+                    {
+                        Debug.WriteLine("Error in rendering in DirectXHost: " + exc.Message);
+                    }
+                    finally
+                    {
+                        _image.Unlock();
+                    }
+                }
+            }
+            catch (SharpDXException exc)
+            {
+                Device.Reset(_pp);
+                Debug.WriteLine("Exception in main render loop: " + exc.Message);
+            }
+        }
 
-		public event EventHandler<RenderEventArgs> Render;
+        public event EventHandler<RenderEventArgs> Render;
 
-		protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
-		{
-			drawingContext.DrawImage(_image, new Rect(RenderSize));
-		}
+        protected override void OnRender(System.Windows.Media.DrawingContext drawingContext)
+        {
+            drawingContext.DrawImage(_image, new Rect(RenderSize));
+        }
 
-		#region IPlotterElement Members
+        #region IPlotterElement Members
 
-		private Plotter2D _plotter;
-		public void OnPlotterAttached(Plotter plotter)
-		{
-			_plotter = (Plotter2D)plotter;
-			_plotter.CentralGrid.Children.Add(this);
-			_plotter.Viewport.PropertyChanged += Viewport_PropertyChanged;
-			Initialize3D();
-		}
+        private Plotter2D _plotter;
+        public void OnPlotterAttached(Plotter plotter)
+        {
+            _plotter = (Plotter2D)plotter;
+            _plotter.CentralGrid.Children.Add(this);
+            _plotter.Viewport.PropertyChanged += Viewport_PropertyChanged;
 
-		private void Viewport_PropertyChanged(object sender, ExtendedPropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == "Visible")
-			{
-				if (e.NewValue is DataRect newRect)
-				{
-					// Change the matrix
-				}
-			}
-		}
+            Initialize3D();
 
-		public void OnPlotterDetaching(Plotter plotter)
-		{
-			_plotter.Viewport.PropertyChanged -= Viewport_PropertyChanged;
-			_plotter.CentralGrid.Children.Remove(this);
-			_plotter = null;
-			Device.Dispose();
-			_direct3D.Dispose();
-		}
+            if (_plotter.IsVisible && !_compositionTargetRenderingHandlerAdded)
+            {
+                //System.Windows.Media.CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
+                CompositionTargetRenderingEventManager.AddHandler(CompositionTarget_Rendering);
+                _compositionTargetRenderingHandlerAdded = true;
+            }
+            _plotter.IsVisibleChanged += (s, e) =>
+            {
+                if (_plotter.IsVisible && !_compositionTargetRenderingHandlerAdded)
+                {
+                    //System.Windows.Media.CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
+                    CompositionTargetRenderingEventManager.AddHandler(CompositionTarget_Rendering);
+                    _compositionTargetRenderingHandlerAdded = true;
+                }
+                if (!_plotter.IsVisible && _compositionTargetRenderingHandlerAdded)
+                {
+                    CompositionTargetRenderingEventManager.RemoveHandler(CompositionTarget_Rendering);
+                    _compositionTargetRenderingHandlerAdded = false;
+                }
+            };
+       }
 
-		public Plotter Plotter => _plotter;
+        private void Viewport_PropertyChanged(object sender, ExtendedPropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Visible")
+            {
+                if (e.NewValue is DataRect newRect)
+                {
+                    // Change the matrix
+                }
+            }
+        }
 
-		#endregion
+        public void OnPlotterDetaching(Plotter plotter)
+        {
+            if (_compositionTargetRenderingHandlerAdded)
+            {
+                CompositionTargetRenderingEventManager.RemoveHandler(CompositionTarget_Rendering);
+                _compositionTargetRenderingHandlerAdded = false;
+            }
 
-	}
+            _plotter.Viewport.PropertyChanged -= Viewport_PropertyChanged;
+            _plotter.CentralGrid.Children.Remove(this);
+            _plotter = null;
+            Device.Dispose();
+            _direct3D.Dispose();
+        }
 
-	public class RenderEventArgs : EventArgs
-	{
-		public RenderEventArgs(int width, int height)
-		{
-			Width = width;
-			Height = height;
-		}
+        public Plotter Plotter => _plotter;
 
-		public int Width { get; }
-		public int Height { get; }
-	}
-		
+        #endregion
+
+    }
+
+    public class RenderEventArgs : EventArgs
+    {
+        public RenderEventArgs(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+    }
+        
 
 }
