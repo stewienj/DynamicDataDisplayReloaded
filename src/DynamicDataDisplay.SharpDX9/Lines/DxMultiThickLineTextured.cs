6a98acb1463272fd0a5787a065c172e1255806e6
John Stewien <stewienj@users.noreply.github.com>
2022-05-19 16:38:43 +0930

    Added more to DxMultiThickLineTextured

--- src/DynamicDataDisplay.SharpDX9/Lines/DxMultiThickLineTextured.cs
+++ src/DynamicDataDisplay.SharpDX9/Lines/DxMultiThickLineTextured.cs
@@ -3,6 +3,11 @@ using DynamicDataDisplay.SharpDX9.Shaders;
 using SharpDX.Direct3D9;
 using System.Windows;
 using DynamicDataDisplay.SharpDX9.Textures;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using System.Threading;
+using System.Windows.Media;
 
 namespace DynamicDataDisplay.SharpDX9.Lines
 {
@@ -26,8 +31,36 @@ namespace DynamicDataDisplay.SharpDX9.Lines
     }
     */
 
+    /// <summary>
+    /// Placeholder implementation, so this can be used before it is finished.
+    /// </summary>
+
     public class DxMultiThickLineTextured : BaseDxPrimitive<D3Point>
     {
+        public DxMultiThickLineTextured()
+        {
+            this.IsHitTestVisible = true;
+        }
+
+        public override void OnPlotterAttached(Plotter plotter)
+        {
+            base.OnPlotterAttached(plotter);
+            if (_dxLeftMouseButtonDownHandlerCount > 0)
+            {
+                Plotter.MouseLeftButtonDown += Plotter_MouseLeftButtonDown;
+            }
+        }
+
+        public override void OnPlotterDetaching(Plotter plotter)
+        {
+            base.OnPlotterDetaching(plotter);
+            if (_dxLeftMouseButtonDownHandlerCount > 0)
+            {
+                Plotter.MouseLeftButtonDown -= Plotter_MouseLeftButtonDown;
+            }
+        }
+
+
         protected override BaseDxTransformShader GetTransformEffect(Device device)
         {
             return new DxPointSingleColorShader(Device);
@@ -36,6 +69,11 @@ namespace DynamicDataDisplay.SharpDX9.Lines
         protected override PrimitiveType GetPrimitiveType()
         {
             return PrimitiveType.LineList;
+        }
+
+        private void Plotter_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // TODO: Hit testing of the line
         }
 
         public double LineThickness
@@ -69,5 +107,48 @@ namespace DynamicDataDisplay.SharpDX9.Lines
                    // primitive._texture = (e.NewValue as DxSharedTexture)?.GetTexture(primitive.Device);
                 }
             }));
+
+        public IEnumerable<double> DashPattern
+        {
+            get { return (IEnumerable<double>)GetValue(DashPatternProperty); }
+            set { SetValue(DashPatternProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DashPattern.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DashPatternProperty =
+            DependencyProperty.Register("DashPattern", typeof(IEnumerable<double>), typeof(DxMultiThickLineTextured), new PropertyMetadata(Enumerable.Empty<double>()));
+
+
+        // We only hook the Plotter MouseLeftButtonDown and do hit testing if there is a listener
+        // so this keep tabs on whether we have a listener or not
+
+        private int _dxLeftMouseButtonDownHandlerCount = 0;
+        private event MouseButtonEventHandler _dxLeftMouseButtonDown;
+        public event MouseButtonEventHandler DxLeftMouseButtonDown
+        {
+            add
+            {
+                _dxLeftMouseButtonDown += value;
+                if (Interlocked.Increment(ref _dxLeftMouseButtonDownHandlerCount) == 1)
+                {
+                    if (Plotter != null)
+                    {
+                        Plotter.MouseLeftButtonDown += Plotter_MouseLeftButtonDown;
+                    }
+                }
+            }
+            remove
+            {
+                _dxLeftMouseButtonDown -= value;
+
+                if (Interlocked.Decrement(ref _dxLeftMouseButtonDownHandlerCount) == 0)
+                {
+                    if (Plotter != null)
+                    {
+                        Plotter.MouseLeftButtonDown -= Plotter_MouseLeftButtonDown;
+                    }
+                }
+            }
+        }
     }
 }
