75b0e2ebb86cb19cf738fcee0470939d090137a4
John Stewien <stewienj@users.noreply.github.com>
2023-05-05 14:31:20 +0930

    Added some mouse navigation class for selecting areas of the chart

---
+++ src/DynamicDataDisplay.Samples/Demos/Custom/MouseNaviationSwitchingSample.xaml.cs
@@ -0,0 +1,176 @@
+﻿using DynamicDataDisplay.Common.Auxiliary;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DynamicDataDisplay.Samples.Demos.Custom
+{
+    /// <summary>
+    /// Interaction logic for MouseNaviationSwitchingSample.xaml
+    /// </summary>
+    public partial class MouseNaviationSwitchingSample : Page
+    {
+        public MouseNaviationSwitchingSample()
+        {
+            DataContext = new MouseNaviationSwitchingViewModel();
+            InitializeComponent();
+            _chartPlotter.Children.Remove(_chartPlotter.MouseNavigation);
+            // Grabs the focus so when the user clicks the ListBox it works immediately
+            Focus();
+        }
+    }
+
+    // Below is the view model used in the above class's DataContext
+
+    public class MouseNaviationSwitchingViewModel : D3NotifyPropertyChanged
+    {
+        // ********************************************************************
+        // Private Fields
+        // ********************************************************************
+        #region Private Fields
+
+        /// <summary>
+        /// List of keys for selecting a mouse navigation mode
+        /// </summary>
+        private List<string> _keys = new[]
+        {
+            "Default",
+            "Freehand Select Area",
+            "Rectangle Select Area",
+            "Polygon Select Area",
+            "Move Selected Area"
+        }.ToList(); // Convert to list so we can use IndexOf
+
+        /// <summary>
+        /// A set of instructions that match the aboeve keys
+        /// </summary>
+        private string[] _instructionSet = new[]
+        {
+            // Default:
+            "Current Selection = Default\nDefault mode lets you pan the whole plot by dragging it around with the left mouse button.",
+            // Freehand Select Area
+            "Current Selection = Freehand Select Area\nFreehand select area lets you hold down the left mouse button and draw a selection area.",
+            // Rectangle Select Area
+            "Current Selection = Rectangle Select Area\nRectangle select area lets you hold down the left mouse button and drag out rectanglular selection area.",
+            // Polygon Select Area
+            "Current Selection = Polygon Select Area\nPolygon select area lets you click a series of point to draw a polygon to select an area. To stop hit the ESC key.",
+            // Move Selected Area
+            "Current Selection = Move Selected Area\nClick and drag anywhere on the plot to move the selected area."
+        };
+
+        #endregion Private Fields
+
+        // ********************************************************************
+        // Public Methods
+        // ********************************************************************
+
+
+        public MouseNaviationSwitchingViewModel()
+        {
+            // When the selection area changes signal the polygon on the chart to be redrawn
+            SelectionArea.CollectionChanged += (s, e) => RaisePropertyChanged(nameof(PolyLinePoints));
+        }
+
+        // ********************************************************************
+        // Properties
+        // ********************************************************************
+        #region Properties
+
+        /// <summary>
+        /// A list of available keys for selecting a Mouse Navigation mode
+        /// </summary>
+        public List<string> Keys => _keys;
+
+        /// <summary>
+        /// The currently selected Mouse Navigation mode
+        /// </summary>
+        private string _selectedMouseMode = "Default";
+        public string SelectedMouseMode
+        {
+            get => _selectedMouseMode;
+            set
+            {
+                SetProperty(ref _selectedMouseMode, value);
+                RaisePropertyChanged(nameof(SelectedInstructions));
+            }
+        }
+
+        /// <summary>
+        /// Instructions to display in the instruction box
+        /// </summary>
+        public string SelectedInstructions
+        {
+            get
+            {
+                var index = _keys.IndexOf(SelectedMouseMode);
+                if (index < 0 || index >= _instructionSet.Length)
+                {
+                    return "Unknown Selection";
+                }
+                else if (SelectedMouseMode == "Move Selected Area")
+                {
+                    return _instructionSet[index] + MoveStatus;
+
+                }
+                else
+                {
+                    return _instructionSet[index];
+                }
+            }
+        }
+
+        /// <summary>
+        /// When is move area mode, indicates whether the user is moving or not moving the selection area
+        /// </summary>
+        private string _moveStatus = "\nMove Status = Not Moving";
+        public string MoveStatus
+        {
+            get => _moveStatus;
+            set
+            {
+                SetProperty(ref _moveStatus, value);
+                RaisePropertyChanged(nameof(SelectedInstructions));
+            }
+        }
+
+        /// <summary>
+        /// The selection area that gets written to by the Mouse Navigation handlers
+        /// </summary>
+        public ObservableCollection<Point> SelectionArea { get; } = new ObservableCollection<Point>();
+
+        #endregion Properties
+
+        /// <summary>
+        /// A list of points used to draw a polygon
+        /// </summary>
+        public IList<Point> PolyLinePoints => SelectionArea.Concat(SelectionArea.Take(1)).ToList();// Close the polygon
+
+        // ********************************************************************
+        // Commands
+        // ********************************************************************
+        #region Commands
+
+        /// <summary>
+        /// Command that gets called when the user starts moving the selection
+        /// </summary>
+        private RelayCommandFactoryD3 _moveStartedCommand = new RelayCommandFactoryD3();
+        public ICommand MoveStartedCommand => _moveStartedCommand.GetCommand(() =>
+        {
+            MoveStatus = "\nMove Status = Moving";
+        });
+
+        /// <summary>
+        /// Command that gets called when the user stops moving the selection
+        /// </summary>
+        private RelayCommandFactoryD3 _moveStoppedCommand = new RelayCommandFactoryD3();
+        public ICommand MoveStoppedCommand => _moveStoppedCommand.GetCommand(() =>
+        {
+            MoveStatus = "\nMove Status = Not Moving";
+        });
+
+        #endregion Commands
+    }
+}
---
+++ src/DynamicDataDisplay/Charts/Navigation/MouseNavigationMoveSelectedArea.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace DynamicDataDisplay.Navigation
+{
+    public class MouseNavigationMoveSelectedArea : NavigationBase
+    {
+        private IList<Point> _originalPoints = new List<Point>();
+        private Point _firstDataPoint;
+        private Point _lastDataPoint;
+
+        protected override void OnPlotterAttached(Plotter plotter)
+        {
+            base.OnPlotterAttached(plotter);
+
+            Mouse.AddMouseDownHandler(Parent, OnMouseDown);
+            Mouse.AddMouseMoveHandler(Parent, OnMouseMove);
+            Mouse.AddMouseUpHandler(Parent, OnMouseUp);
+            Mouse.AddMouseWheelHandler(Parent, OnMouseWheel);
+
+            plotter.KeyDown += new KeyEventHandler(OnParentKeyDown);
+        }
+
+        protected override void OnPlotterDetaching(Plotter plotter)
+        {
+            plotter.KeyDown -= new KeyEventHandler(OnParentKeyDown);
+
+            Mouse.RemoveMouseDownHandler(Parent, OnMouseDown);
+            Mouse.RemoveMouseMoveHandler(Parent, OnMouseMove);
+            Mouse.RemoveMouseUpHandler(Parent, OnMouseUp);
+            Mouse.RemoveMouseWheelHandler(Parent, OnMouseWheel);
+
+            IsMoving = false;
+
+            base.OnPlotterDetaching(plotter);
+        }
+
+        private void OnParentKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape || e.Key == Key.Back)
+            {
+                if (IsMoving)
+                {
+                    IsMoving = false;
+                    ReleaseMouseCapture();
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && Keyboard.Modifiers == ModifierKeys.None)
+            {
+                var mousePoint = e.GetPosition(this);
+                _firstDataPoint = Viewport.Transform.ScreenToData(mousePoint);
+                _lastDataPoint = _firstDataPoint;
+
+                IsMoving = true;
+
+                _originalPoints = SelectedAreaPath.ToList();
+
+                // not capturing mouse because this made some tools like PointSelector not
+                // receive MouseUp events on markers;
+                // Mouse will be captured later, in the first MouseMove handler call.
+                // CaptureMouse();
+                // e.Handled = true;
+            }
+
+            if (!Plotter.IsFocused)
+            {
+                Plotter.Focus();
+
+                // this is done to prevent other tools like PointSelector from getting mouse click event when clicking on plotter
+                // to activate window it's contained within
+                e.Handled = true;
+            }
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (IsMoving && e.ChangedButton == MouseButton.Left)
+            {
+                IsMoving = false;
+                if (!Plotter.IsFocused)
+                {
+                    Plotter2D.Focus();
+                }
+
+                ReleaseMouseCapture();
+            }
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsMoving)
+                return;
+
+            if (IsMoving && e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (!IsMouseCaptured)
+                {
+                    CaptureMouse();
+                }
+
+                var mousePoint = e.GetPosition(this);
+                _lastDataPoint = Viewport.Transform.ScreenToData(mousePoint);
+                Vector shift = _lastDataPoint - _firstDataPoint;
+
+                SelectedAreaPath.Clear();
+                SelectedAreaPath.AddMany(_originalPoints.Select(p => p + shift));
+
+                e.Handled = true;
+            }
+        }
+
+        private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            if (IsMoving)
+            {
+                IsMoving = false;
+                ReleaseMouseCapture();
+            }
+            base.OnLostFocus(e);
+        }
+
+        public IList<Point> SelectedAreaPath
+        {
+            get { return (IList<Point>)GetValue(SelectedAreaPathProperty); }
+            set { SetValue(SelectedAreaPathProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UserDrawnPath.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedAreaPathProperty =
+            DependencyProperty.Register("SelectedAreaPath", typeof(IList<Point>), typeof(MouseNavigationMoveSelectedArea), new PropertyMetadata(new List<Point>()));
+
+        public bool IsMoving
+        {
+            get { return (bool)GetValue(IsMovingProperty); }
+            set { SetValue(IsMovingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsMoving.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsMovingProperty =
+            DependencyProperty.Register("IsMoving", typeof(bool), typeof(MouseNavigationMoveSelectedArea), new PropertyMetadata(false, (s, e) =>
+            {
+                if (s is MouseNavigationMoveSelectedArea nav)
+                {
+                    if (e.NewValue is bool newValue && e.OldValue is bool oldValue)
+                    {
+                        if (newValue && !oldValue)
+                        {
+                            nav.StartedMoving?.Execute(nav._firstDataPoint);
+                        }
+                        if (!newValue && oldValue)
+                        {
+                            nav.StoppedMoving?.Execute(nav._lastDataPoint);
+                        }
+                    }
+                }
+            }));
+
+
+        public ICommand StartedMoving
+        {
+            get { return (ICommand)GetValue(StartedMovingProperty); }
+            set { SetValue(StartedMovingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StartedMoving.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StartedMovingProperty =
+            DependencyProperty.Register("StartedMoving", typeof(ICommand), typeof(MouseNavigationMoveSelectedArea), new PropertyMetadata(null));
+
+        public ICommand StoppedMoving
+        {
+            get { return (ICommand)GetValue(StoppedMovingProperty); }
+            set { SetValue(StoppedMovingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StartedMoving.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StoppedMovingProperty =
+            DependencyProperty.Register("StoppedMoving", typeof(ICommand), typeof(MouseNavigationMoveSelectedArea), new PropertyMetadata(null));
+
+
+    }
+}
