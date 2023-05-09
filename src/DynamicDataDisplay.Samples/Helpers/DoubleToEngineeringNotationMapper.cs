60c1313af1d30cfd5e53726b44f28787ca60c3d5
John Stewien <stewienj@users.noreply.github.com>
2023-05-09 12:22:33 +0930

    Improved on the FrequencyTimelineChart example in the samples project

---
+++ src/DynamicDataDisplay.Samples/Helpers/DoubleToEngineeringNotationMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace DynamicDataDisplay.Samples.Helpers
+{
+    /// <summary>
+    /// XAML mark up extension to convert a double value into a string value.
+    /// This is a singleton so you don't have to instantiate it in your resources.
+    /// This allows you to use your mapper like this inside your binding expression.
+    /// Converter={sf:DoubleToEngineeringNotationMapper}}
+    /// </summary>
+    [MarkupExtensionReturnType(typeof(Func<double, string>))]
+    public class DoubleToEngineeringNotationMapper : MarkupExtension
+    {
+        private static readonly Lazy<DoubleToEngineeringNotationMapper> _mapper = new Lazy<DoubleToEngineeringNotationMapper>(true);
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _mapper.Value.GetMapper();
+        }
+
+        public static DoubleToEngineeringNotationMapper GetInstance() => _mapper.Value;
+
+        private Func<double, string> GetMapper()
+        {
+            Func<double, string> mapper = d => d.ToEngineeringNotation() + "Hz";
+
+            return mapper;
+        }
+    }
+}
---
+++ src/DynamicDataDisplay.Samples/Helpers/MinutesToUTCStringMapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace DynamicDataDisplay.Samples.Helpers
+{
+    /// <summary>
+    /// XAML mark up extension to convert a double value into a string value.
+    /// This is a singleton so you don't have to instantiate it in your resources.
+    /// This allows you to use your mapper like this inside your binding expression.
+    /// Converter={sf:MinutesToUTCStringMapper}}
+    /// </summary>
+    [MarkupExtensionReturnType(typeof(Func<double, string>))]
+    public class MinutesToUTCStringMapper : MarkupExtension
+    {
+        private static readonly Lazy<MinutesToUTCStringMapper> _mapper = new Lazy<MinutesToUTCStringMapper>(true);
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _mapper.Value.GetMapper();
+        }
+
+        public static MinutesToUTCStringMapper GetInstance() => _mapper.Value;
+
+        private Func<double, string> GetMapper()
+        {
+            Func<double, string> mapper = d =>
+            {
+                var dateTime = new DateTime();
+                if (d > 0.0)
+                {
+                    dateTime = dateTime.AddMinutes(d);
+                }
+
+                dateTime = dateTime.ToUniversalTime();
+
+                return dateTime.ToString("dd MMM yyyy HH:mm");
+            };
+
+            return mapper;
+        }
+    }
+}
